d4959ae9270109f1c3b45bcd67f9525f978db76d
Wolfgang Schnerring <wosc@wosc.de>
2013-02-18 14:55:27 +0100

    Preparing release 1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.month',
-    version='1.2.dev0',
+    version='1.2',
     author='gocept gmbh & co. kg',
     author_email='mail@gocept.com',
     url='http://pypi.python.org/pypi/gocept.month',

08f36ac73768eb505dde2018f592306562c43e1f
Steffen Allner <sa@gocept.com>
2019-10-21 11:43:17 +0200

    Support all current Python versions.
    Drop support for 3.4 and claim it for 3.7 and  3.8.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.month',
-    version='2.2.dev0',
+    version='3.0.dev0',
     author='gocept gmbh & co. kg',
     author_email='mail@gocept.com',
     url='https://github.com/gocept/gocept.month',
@@ -26,9 +26,10 @@ Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
 Programming Language :: Python :: Implementation :: CPython
 Programming Language :: Python :: Implementation :: PyPy
 Development Status :: 5 - Production/Stable
